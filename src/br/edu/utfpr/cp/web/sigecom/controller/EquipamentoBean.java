1c1b10860701a46830a82237d5a4f9c0de956d42
lyncolnms <lyncolnms@gmail.com>
2014-10-13 20:41:30 -0300

    Commit 13/10/2014 20:41

---
+++ src/br/edu/utfpr/cp/web/sigecom/controller/EquipamentoBean.java
@@ -0,0 +1,121 @@
+package br.edu.utfpr.cp.web.sigecom.controller;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.primefaces.event.RowEditEvent;
+
+import br.edu.utfpr.cp.web.sigecom.model.Equipamento;
+
+@Named
+@Stateful
+@ViewScoped
+public class EquipamentoBean {
+
+	private Equipamento equipamentos;
+	private List<Equipamento> filtroEquipamentos;
+	private Collection<Equipamento> listaEquipamentos;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	@PostConstruct
+	public void init() {
+		this.equipamentos = new Equipamento();
+		Query query = em
+				.createQuery("SELECT u FROM Equipamento u ORDER BY u.nome");
+		this.listaEquipamentos = query.getResultList();
+	}
+
+	public Equipamento getEquipamentos() {
+		return equipamentos;
+	}
+
+	public void setEquipamentos(Equipamento equipamentos) {
+		this.equipamentos = equipamentos;
+	}
+
+	public List<Equipamento> getFiltroEquipamentos() {
+		return filtroEquipamentos;
+	}
+
+	public void setFiltroEquipamentos(List<Equipamento> filtroEquipamentos) {
+		this.filtroEquipamentos = filtroEquipamentos;
+	}
+
+	public Collection<Equipamento> getListaEquipamentos() {
+		return listaEquipamentos;
+	}
+
+	public void setListaEquipamentos(Collection<Equipamento> listaEquipamentos) {
+		this.listaEquipamentos = listaEquipamentos;
+	}
+
+	public void salvar(Equipamento equip) {
+
+		try {
+			this.em.persist(equip);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.init();
+		}
+
+	}
+
+	public void excluir(Equipamento equip) {
+
+		try {
+			Equipamento equipamento = this.em.find(Equipamento.class,
+					equip.getNome());
+			this.em.remove(equipamento);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.init();
+		}
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+
+		this.equipamentos = (Equipamento) event.getObject();
+
+		FacesMessage msg = new FacesMessage("Equipamento Editado",
+				equipamentos.getNome());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		// Faz alteração no usuário da linha selecionada
+		try {
+			em.merge(equipamentos);
+			em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Equipamento Cancelado",
+				((Equipamento) event.getObject()).getNome());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void removeMessage() {
+		FacesMessage msg = new FacesMessage("Equipamento Removido",
+				equipamentos.getNome());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+}
---
+++ src/br/edu/utfpr/cp/web/sigecom/model/Equipamento.java
@@ -0,0 +1,67 @@
+package br.edu.utfpr.cp.web.sigecom.model;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+
+import javax.persistence.*;
+
+@Entity
+public class Equipamento implements Serializable {
+
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String nome;
+	
+	private static final long serialVersionUID = 1L;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}   
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipamento other = (Equipamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+   
+}
