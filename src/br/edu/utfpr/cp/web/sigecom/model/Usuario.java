633ee1cb33114479c4bc20c373c3e1f4395908c5
lyncolnms <lyncolnms@gmail.com>
2014-11-03 20:42:10 -0200

    Entrega Sprint E2.

--- src/br/edu/utfpr/cp/web/sigecom/model/Usuario.java
+++ src/br/edu/utfpr/cp/web/sigecom/model/Usuario.java
@@ -11,31 +11,26 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	private String username;
+	private String login;
 
-	private String password;
+	private String senha;
 
 	private String nomeCompleto;
 
-	@OneToOne
-	private Autorizacao autorizacao;
-
-	private boolean enable;
-
-	public String getUsername() {
-		return username;
+	public String getLogin() {
+		return login;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setLogin(String login) {
+		this.login = login;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getSenha() {
+		return senha;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getNomeCompleto() {
@@ -46,35 +41,14 @@ public class Usuario implements Serializable {
 		this.nomeCompleto = nomeCompleto;
 	}
 
-	public Autorizacao getAutorizacao() {
-		return autorizacao;
-	}
-
-	public void setAutorizacao(Autorizacao autorizacao) {
-		this.autorizacao = autorizacao;
-	}
-
-	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((autorizacao == null) ? 0 : autorizacao.hashCode());
-		result = prime * result + (enable ? 1231 : 1237);
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		result = prime * result
 				+ ((nomeCompleto == null) ? 0 : nomeCompleto.hashCode());
-		result = prime * result
-				+ ((password == null) ? 0 : password.hashCode());
-		result = prime * result
-				+ ((username == null) ? 0 : username.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		return result;
 	}
 
@@ -87,29 +61,21 @@ public class Usuario implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
-		if (autorizacao == null) {
-			if (other.autorizacao != null)
+		if (login == null) {
+			if (other.login != null)
 				return false;
-		} else if (!autorizacao.equals(other.autorizacao))
-			return false;
-		if (enable != other.enable)
+		} else if (!login.equals(other.login))
 			return false;
 		if (nomeCompleto == null) {
 			if (other.nomeCompleto != null)
 				return false;
 		} else if (!nomeCompleto.equals(other.nomeCompleto))
 			return false;
-		if (password == null) {
-			if (other.password != null)
+		if (senha == null) {
+			if (other.senha != null)
 				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
+		} else if (!senha.equals(other.senha))
 			return false;
 		return true;
 	}
-
 }
